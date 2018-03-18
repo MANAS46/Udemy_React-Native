d0a7e5d0f56e206c28ad888f6fc336c83679fd44
Mohamed Farhath Manas K M <k_manas@rocketmail.com>
2018-03-19 00:19:12 +0530

    First commit

---
+++ Albums/index.js
@@ -0,0 +1,14 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('Albums', () => App);
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Header } from './src/components/header';
+
+const App = () => (
+  <Header />
+);
+
+AppRegistry.registerComponent('Albums', () => App);
